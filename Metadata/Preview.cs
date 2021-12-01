f9ef83c3ab5b25c60efeda9b188a19e9868f8cdb
sao.vu <quangsaobk@gmail.com>
2021-12-01 12:58:45 +0800

    add unit test, using dependency injection

--- Metadata/Preview.cs
+++ Metadata/Preview.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
+using System.Web;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
 using Microsoft.Playwright;
-using System.Text.Json;
 
 namespace Metadata
 {
@@ -18,46 +20,68 @@ namespace Metadata
         public string Func { get; init; }
         public string Param { get; init; }
     }
-    
-    public static class Preview
+
+    public interface IPreviewResponse
     {
-        [Function("Preview")]
-        public static async Task<HttpResponseData> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", Route = "preview")] HttpRequestData req,
-            string url,
-            FunctionContext executionContext)
+        public string Url { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+    }
+
+    public record PreviewResponse() : IPreviewResponse
+    {
+        public string Url { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+    }
+
+    public class Preview
+    {
+        private readonly ILogger<Preview> _logger;
+
+        public Preview(ILogger<Preview> logger)
         {
-            var logger = executionContext.GetLogger("Hook");
-            logger.LogInformation("C# HTTP trigger function processed a request.");
+            _logger = logger;
+        }
+        
+        [Function("Preview")]
+        public async Task<HttpResponseData> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = "preview")] HttpRequestData req)
+        {
+            this._logger.LogInformation("C# HTTP trigger function processed a request.");
+            
+            var queryParameters = HttpUtility.ParseQueryString(req.Url.Query);
+            var url = queryParameters.Get("url");
 
             // validate url
-            if (!ValidateUrl(url))
+            if (url == null || !ValidateUrl(url))
             {
                 return req.CreateResponse(HttpStatusCode.BadRequest);
             }
 
             // check allowed hosts
             string[] allowedHost = {"www.bandlab.com"};
-            Uri hostUri = new Uri(url);   
-            string host = hostUri.Host;
+            var hostUri = new Uri(url);   
+            var host = hostUri.Host;
 
             if (!allowedHost.Contains(host))
             {
                 return req.CreateResponse(HttpStatusCode.BadRequest);
             }
 
-            var map = await ExtractMetadata(url);
+            var result = await ExtractMetadata(url);
+            Console.WriteLine(result.Title);
             
             var response = req.CreateResponse(HttpStatusCode.OK);
-            response.Headers.Add("Content-Type", "application/json; charset=utf-8");
-            string jsonString = JsonSerializer.Serialize(map);  
-            await response.WriteStringAsync(jsonString);
-
+            response.Headers.Add("Content-Type", "application/json");
+            string jsonString = JsonSerializer.Serialize(result);
+            await response.WriteStringAsync(jsonString, Encoding.UTF8);
             return response;
-            
         }
 
-        private static async Task<Dictionary<string, string>> ExtractMetadata(string url)
+        private static async Task<IPreviewResponse> ExtractMetadata(string url)
         {
             using var playwright = await Playwright.CreateAsync();
             await using var browser = await playwright.Chromium.LaunchAsync();
@@ -66,16 +90,15 @@ namespace Metadata
 
             MetaSelector[] selectors =
             {
-                new MetaSelector() {KeyName = "title", Selector = "head>title", Func = "InnerTextAsync"},
-                new MetaSelector() {KeyName = "description", Selector = "head>meta[name=\"description\"]", Func = "GetAttributeAsync", Param = "content"},
-                new MetaSelector() {KeyName = "image", Selector = "head>meta[property=\"og:image\"]", Func = "GetAttributeAsync", Param = "content"},
+                new MetaSelector() {KeyName = nameof(PreviewResponse.Title), Selector = "head>title", Func = "InnerTextAsync"},
+                new MetaSelector() {KeyName = nameof(PreviewResponse.Description), Selector = "head>meta[name=\"description\"]", Func = "GetAttributeAsync", Param = "content"},
+                new MetaSelector() {KeyName = nameof(PreviewResponse.Image), Selector = "head>meta[property=\"og:image\"]", Func = "GetAttributeAsync", Param = "content"},
             };
-            var map = new Dictionary<string, string>() {{"url", url}};
-            
+            var map = new Dictionary<string, string>() {};
             foreach (MetaSelector metaSelector in selectors)
             {
                 var element = await page.QuerySelectorAsync(metaSelector.Selector);
-                var value = "";
+                string value = null;
                 if (element != null)
                 {
                     if (metaSelector.Func == "InnerTextAsync")
@@ -90,8 +113,15 @@ namespace Metadata
 
                 map.Add(metaSelector.KeyName, value);
             }
+            var result = new PreviewResponse
+            {
+                Url = url,
+                Title = map[nameof(PreviewResponse.Title)],
+                Description = map[nameof(PreviewResponse.Description)],
+                Image = map[nameof(PreviewResponse.Image)],
+            };
             await browser.CloseAsync();
-            return map;
+            return result;
         }
         
         private static bool ValidateUrl(string url)
---
+++ Metadata/Test.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using Moq;
+using Xunit;
+
+namespace Metadata
+{
+    public class Tests
+    {
+        [Fact]
+        public async Task ShouldReturnOk()
+        {
+            
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddScoped<ILoggerFactory, LoggerFactory>();
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+
+            var context = new Mock<FunctionContext>();
+            context.SetupProperty(c => c.InstanceServices, serviceProvider);
+            
+            var requestObj = new Mock<HttpRequestData>(context.Object);
+            
+            requestObj.Setup(r => r.Url).Returns(new Uri("http://localhost:7071/api/Preview?url=https%3A%2F%2Fwww.bandlab.com%2Feumonik"));
+            
+            requestObj.Setup(r => r.CreateResponse()).Returns(() =>
+            {
+                var response = new Mock<HttpResponseData>(context.Object);
+                response.SetupProperty(r => r.Headers, new HttpHeadersCollection());
+                response.SetupProperty(r => r.StatusCode);
+                response.SetupProperty(r => r.Body, new MemoryStream());
+                return response.Object;
+            });
+            
+            var req = requestObj.Object;
+            var logger = NullLoggerFactory.Instance.CreateLogger<Metadata.Preview>();
+            var function = new Preview(logger);
+            var response = await function.Run(req);
+
+            Assert.NotNull(response);
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+    }
+}
